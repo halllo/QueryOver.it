2887651a30ba4eacc3b770266aa82ae3b9f4bd4e
Manuel Naujoks <mnaujoks@gmail.com>
2013-04-05 14:00:17 +0200

    initialize and cleanup pulled to base class

---
+++ QueryOverSamples/QueryOverSamples/SimpleTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueryOverSamples.Domain;
+using System.Linq;
+
+namespace QueryOverSamples
+{
+	[TestClass]
+	public class SimpleTests : SessionTests
+	{
+		[TestMethod]
+		public void SaveAndRetrieve()
+		{
+			Session.Save(new Person { Name = "Max" });
+
+			var query = Session.QueryOver<Person>();
+			var persons = query.List();
+
+			Assert.IsTrue(persons.Any(p => p.Name == "Max"));
+		}
+
+		[TestMethod]
+		public void RowCount()
+		{
+			Session.Save(new Person { Name = "Max1" });
+			Session.Save(new Person { Name = "Max2" });
+
+			var query = Session.QueryOver<Person>();
+			var personsCount = query.RowCount();
+
+			Assert.AreEqual(2, personsCount);
+		}
+	}
+}
