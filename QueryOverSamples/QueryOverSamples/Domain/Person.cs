676ada4745fb0c771c1a13ea2842839aa9ab35e6
Johannes <johannes.lipsky@gmail.com>
2013-05-01 17:46:12 +0200

    Added Simple Tests

--- QueryOverSamples/QueryOverSamples/Domain/Person.cs
+++ QueryOverSamples/QueryOverSamples/Domain/Person.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System;
 namespace QueryOverSamples.Domain
 {
 	public class Person
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? DateOfBirth { get; set; } 
 	}
 }
