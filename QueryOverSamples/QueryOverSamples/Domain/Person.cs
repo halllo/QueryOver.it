e8343ee817a1ed25a51a16462b6c4186ab598311
Manuel Naujoks <mnaujoks@gmail.com>
2014-03-29 00:40:08 +0100

    tabs instead of spaces

--- QueryOverSamples/QueryOverSamples/Domain/Person.cs
+++ QueryOverSamples/QueryOverSamples/Domain/Person.cs
@@ -1,15 +1,14 @@
-﻿
-namespace QueryOverSamples.Domain
+﻿namespace QueryOverSamples.Domain
 {
-    public interface IPerson
-    {
-        int Id { get; set; }
-        string Name { get; set; }
-    }
+	public interface IPerson
+	{
+		int Id { get; set; }
+		string Name { get; set; }
+	}
 
-    public class Person : IPerson
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+	public class Person : IPerson
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
 }
--- QueryOverSamples/QueryOverSamples/Tests.cs
+++ QueryOverSamples/QueryOverSamples/Tests.cs
@@ -7,44 +7,42 @@ using System.Linq;
 
 namespace QueryOverSamples
 {
-    [TestClass]
-    public class Tests
-    {
-        #region Initialize and Cleanup
-        ISession Session;
+	[TestClass]
+	public class Tests
+	{
+		private ISession Session;
 
-        [TestInitialize]
-        public void Initialize()
-        {
-            var connectionString = new ConnectionString("..\\..\\Database.sdf");
-            var config = Fluently.Configure()
-                .Database(MsSqlCeConfiguration.Standard.ConnectionString(connectionString.ToString()))
-                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Person>())
-                .ExposeConfiguration(x => x.SetProperty("connection.release_mode", "on_close"))
-                .BuildConfiguration();
+		[TestInitialize]
+		public void Initialize()
+		{
+			var connectionString = new ConnectionString("..\\..\\Database.sdf");
+			var config = Fluently.Configure()
+				.Database(MsSqlCeConfiguration.Standard.ConnectionString(connectionString.ToString()))
+				.Mappings(m => m.FluentMappings.AddFromAssemblyOf<Person>())
+				.ExposeConfiguration(x => x.SetProperty("connection.release_mode", "on_close"))
+				.BuildConfiguration();
 
-            connectionString.CreateDatabase(config);
+			connectionString.CreateDatabase(config);
 
-            var sessions = config.BuildSessionFactory();
-            Session = sessions.OpenSession();
-        }
+			var sessions = config.BuildSessionFactory();
+			Session = sessions.OpenSession();
+		}
 
-        [TestCleanup]
-        public void Cleanup()
-        {
-            Session.Dispose();
-        } 
-        #endregion
+		[TestCleanup]
+		public void Cleanup()
+		{
+			Session.Dispose();
+		}
 
-        [TestMethod]
-        public void SaveAndRetrieve()
-        {
-            Session.Save(new Person { Name = "Max" });
+		[TestMethod]
+		public void SaveAndRetrieve()
+		{
+			Session.Save(new Person { Name = "Max" });
 
-            var query = Session.QueryOver<Person>();
-            var persons = query.List();
+			var query = Session.QueryOver<Person>();
+			var persons = query.List();
 
-            Assert.IsTrue(persons.Any(p => p.Name == "Max"));
-        }
-    }
+			Assert.IsTrue(persons.Any(p => p.Name == "Max"));
+		}
+	}
 }
